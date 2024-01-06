15012913623dac2e4f36fcfac94a65192620fd25
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-01-06 17:01:35 +0530

    changes accordiing to the mobile

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import styled from "styled-components";
 import { Link as LinkR, BrowserRouter as Router } from "react-router-dom";
 import { DiCssdeck } from 'react-icons/di';
+import { FaBars } from 'react-icons/fa';
+import { Bio } from '../../data/constants';
+import { Close, CloseRounded } from '@mui/icons-material';
+import { useTheme } from 'styled-components';
 const Nav = styled.nav`
   background-colour: ${({ theme }) => theme.card_light};
   height: 80px;
@@ -125,30 +129,55 @@ export const Span = styled.div`
 `;
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const theme = useTheme()
   return (
     <Nav>
-      <Router>
-        <NavContainer>
-          <NavLogo to="/">
+      <NavbarContainer>
+        <NavLogo to='/'>
           <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }}>
-            <DiCssdeck size="3rem" /> <Span>Welcome</Span>
+            <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
           </a>
-          </NavLogo>
-          <MobileIcon></MobileIcon>
-          <NavItems>
-            <NavLink href="#about">About</NavLink>
-            <NavLink href="#skills">Skills</NavLink>
-            <NavLink href="#projects"> Projects</NavLink>
-            <NavLink href="#education">Education</NavLink>
-            <NavLink href="#experience">Experience</NavLink>
-          </NavItems>
-          <ButtonContainer>
-            <GitHubButton>GitHub</GitHubButton>
-          </ButtonContainer>
-        </NavContainer>
-      </Router>
+        </NavLogo>
+        <MobileIcon>
+          <FaBars onClick={() => {
+            setIsOpen(!isOpen)
+          }} />
+        </MobileIcon>
+        <NavItems>
+          <NavLink href="#about">About</NavLink>
+          <NavLink href='#skills'>Skills</NavLink>
+          <NavLink href='#experience'>Experience</NavLink>
+          <NavLink href='#projects'>Projects</NavLink>
+          <NavLink href='#education'>Education</NavLink>
+        </NavItems>
+        <ButtonContainer>
+          <GitHubButton href={Bio.github} target="_blank">Github Profile</GitHubButton>
+        </ButtonContainer>
+        {
+          isOpen &&
+          <MobileMenu isOpen={isOpen}>
+            <MobileLink href="#about" onClick={() => {
+              setIsOpen(!isOpen)
+            }}>About</MobileLink>
+            <MobileLink href='#skills' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Skills</MobileLink>
+            <MobileLink href='#experience' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Experience</MobileLink>
+            <MobileLink href='#projects' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Projects</MobileLink>
+            <MobileLink href='#education' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Education</MobileLink>
+            <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.github} target="_blank">Github Profile</GitHubButton>
+          </MobileMenu>
+        }
+      </NavbarContainer>
     </Nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
