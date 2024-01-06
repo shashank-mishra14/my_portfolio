eefefee2e7f4488a8f5440301df5f78636d0be76
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-01-07 01:19:35 +0530

    changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -87,7 +87,7 @@ function Footer() {
   return (
     <FooterContainer>
       <FooterWrapper>
-        <Logo>Rishav Chanda</Logo>
+        <Logo>Shashank Mishra</Logo>
         <Nav>
           <NavLink href="#about">About</NavLink>
           <NavLink href="#skills">Skills</NavLink>
@@ -109,7 +109,7 @@ function Footer() {
             <InstagramIcon />
           </SocialMediaIcon>
         </SocialMediaIcons>
-        <Copyright>&copy; 2023 Rishav Chanda. All rights reserved.</Copyright>
+        <Copyright>&copy; 2023 Shashank Mishra. All rights reserved.</Copyright>
       </FooterWrapper>
     </FooterContainer>
   );
--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/shashank-mishra14",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://drive.google.com/file/d/1ZCr6yBijlxZXO6N5egFrIk4HNZIRhHQ5/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/shashank-mishra14/",
   twitter: "https://twitter.com/Sshashankk01",
   insta: "https://www.instagram.com/shashank.sol/",
@@ -99,7 +99,7 @@ export const skills = [
     ],
   },
   {
-    title: "Blockchaim",
+    title: "Blockchain",
     skills: [
       {
         name: "Rust",
