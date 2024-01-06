bd51b65cbef83e1cd53666f8da7a5e97765f21e0
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-01-06 13:38:36 +0530

    chaging the data

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,14 +1,14 @@
 export const Bio = {
-    name: "Rishav Chanda",
+    name: "Shashank Mishra",
     roles: [
       "Full Stack Developer",
-      "Android Developer",
-      "UI/UX Designer",
+      "Competitive Programmer",
+      "Blockchain Developer",
       "Programmer",
     ],
     description:
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-    github: "https://github.com/rishavchanda",
+    github: "https://github.com/shashank-mishra14",
     resume:
       "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
